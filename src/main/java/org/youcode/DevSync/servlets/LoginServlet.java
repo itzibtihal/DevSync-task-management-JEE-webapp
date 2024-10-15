706dd8bd364771b45b1f77347497a45c9a0b501d
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-10-15 22:06:48 +0200

    update LoginServlet

--- src/main/java/org/youcode/DevSync/servlets/LoginServlet.java
+++ src/main/java/org/youcode/DevSync/servlets/LoginServlet.java
@@ -6,15 +6,35 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.mindrot.jbcrypt.BCrypt;
+import org.youcode.DevSync.dao.interfaces.RequestDAO;
+import org.youcode.DevSync.dao.interfaces.TokenManagerDAO;
 import org.youcode.DevSync.dao.interfaces.UserDAO;
+import org.youcode.DevSync.dao.impl.RequestDAOImpl; // Import your actual DAO implementation
+import org.youcode.DevSync.dao.impl.TokenManagerDAOImpl; // Import your actual DAO implementation
 import org.youcode.DevSync.dao.impl.UserDAOImpl;
 import org.youcode.DevSync.domain.entities.User;
+import org.youcode.DevSync.Scheduler.TokenGrantScheduler;
 
 import java.io.IOException;
 import java.util.Optional;
 
 public class LoginServlet extends HttpServlet {
-    private UserDAO userDAO = new UserDAOImpl();
+
+    private UserDAO userDAO; // Declare UserDAO
+    private TokenGrantScheduler tokenGrantScheduler; // Declare TokenGrantScheduler
+    private RequestDAOImpl requestDAO; // Declare RequestDAO
+    private TokenManagerDAOImpl tokenManagerDAO; // Declare TokenManagerDAO
+
+    @Override
+    public void init() throws ServletException {
+        this.userDAO = new UserDAOImpl(); // Instantiate UserDAO
+        this.requestDAO = new RequestDAOImpl(); // Instantiate RequestDAO
+        this.tokenManagerDAO = new TokenManagerDAOImpl(); // Instantiate TokenManagerDAO
+
+        // Instantiate the scheduler with the DAO instances
+        this.tokenGrantScheduler = new TokenGrantScheduler(requestDAO,  tokenManagerDAO);
+        this.tokenGrantScheduler.startScheduler(); // Start the scheduler
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -54,7 +74,6 @@ public class LoginServlet extends HttpServlet {
                 request.getRequestDispatcher("index.jsp").forward(request, response);
             }
         } else {
-
             request.setAttribute("errorMessage", "Invalid username or password");
             request.getRequestDispatcher("index.jsp").forward(request, response);
         }
